dd6d2a7a0d8d8df3d436a34164635a44b875fb66
Thorkil <schjodt.thorkil@gmail.com>
2023-02-19 14:35:35 +0100

    completed 8. starting 9

--- src/08.py
+++ src/08.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+import numpy
 import math
 
 input = list(map(str.rstrip, open('../input/08.txt', 'r')))
@@ -26,50 +27,77 @@ def visible_vertical(input, x, y):
     return visible
 
 def get_left(line, x):
-    left = line[:x]
+
+    left = map(int, line[:x])
     return left
 
 def get_right(line, x):
-    right = line[x+1:]
+    right = map(int, line[x+1:])
     return right
 
 def get_above(input, x, y):
     above = []
     for line in input[:y]:
-        above.append(line[x])
+        above.append(int(line[x]))
     return above
 
 def get_below(input, x, y):
     below = []
     for line in input[y+1:]:
-        below.append(line[x])
+        below.append(int(line[x]))
     return below
 
 def part2_data(input):
     tree_hash = {}
-
     for y in range(len(input)):
         for x in range(len(input[y])):
-            tree_hash[(x, y)] = {
-                    'node': input[y][x],
-                    'above': get_above(input, x, y),
-                    'below': get_below(input, x, y),
-                    'left': get_left(input[y], x),
-                    'right': get_right(input[y], x),
+            tree = {
+                    'node': int(input[y][x]),
+                    'above': list(get_above(input, x, y)),
+                    'below': list(get_below(input, x, y)),
+                    'left': list(get_left(input[y], x)),
+                    'right': list(get_right(input[y], x)),
                     'scenic': 0
                     }
+
+            if tree['left'] and len(tree['left']) > 1:
+                tree['left'].reverse()
+
+            if tree['above'] and len(tree['above']) > 1:
+                tree['above'].reverse()
+
+            tree_hash[(x, y)] = tree
+
 
     return tree_hash
 
 def part2_solve(data):
+    most_scenic = data[(0, 0)]
     size = int(math.sqrt(len(data)))
     for y in range(size):
         for x in range(size):
             tree = data[(x, y)]
-            print(tree)
+            get_scenic(tree)
+            if tree['scenic'] > most_scenic['scenic']:
+                most_scenic = tree
+
+    pprint(most_scenic)
+    
+#TODO: reverse left (and top?)
+def get_scenic(tree):
+    most_scenic = [0, 0, 0, 0]
+    directions = [tree['left'], tree['above'], tree['right'], tree['below']]
+    for index, direction in enumerate(directions):
+        for height in direction:
+            most_scenic[index] = most_scenic[index] + 1
+            if height >= tree['node']:
+                break
+    tree['scenic'] = most_scenic[0] * most_scenic[1] * most_scenic[2] * most_scenic[3]
+    print(tree)
+    print(most_scenic)
+
 
 def part1(input):
-
     tree_hash = {}
 
     for i in range(len(input)):
@@ -90,5 +118,5 @@ def part1(input):
     print(visible_trees)
 
 if __name__ == "__main__":
-    data = part2_data(test_input)
+    data = part2_data(input)
     part2_solve(data)
---
+++ src/09.py
@@ -0,0 +1,9 @@
+def read_input():
+    return list(open('../input/09.txt', 'r'))
+
+if __name__ == "__main__":
+    input = read_input()
+    for line in input:
+        print(line)
+
+
